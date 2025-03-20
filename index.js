5fb88b26f36eeeff88b5d75e3ecb2d8805dcfc2d
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-03-20 18:12:05 +0300

    Completed assignment on strings

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser='KARL KAMAU';
+let welcomeMessage='Welcome to Flatbook, '+ currentUser + '!';
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ currentUser + '!';
+let shortGreeting='Welcome, '+ currentUser[0] + '!'; 
